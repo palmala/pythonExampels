59d2447be8397978e5cf36a3cdbb804d5da56d91
palmala <palmala@users.noreply.github.com>
2021-11-07 21:46:19 +0100

    Counting function calls with decorators

---
+++ decorators/update_wrapper_example.py
@@ -0,0 +1,26 @@
+from functools import update_wrapper
+
+
+class Count:
+    def __init__(self, func):
+        update_wrapper(self, func)
+        self.func = func
+        self.count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        print(f'Current count: {self.count}')
+        result = self.func(*args, **kwargs)
+        return result
+
+
+@Count
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == "__main__":
+    print(fib(20))
