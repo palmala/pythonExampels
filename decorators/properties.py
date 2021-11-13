01ed92ee6f7c835b8d8cea85c52d2f9cbe486a3a
palmala <palmailaszloprivate@gmail.com>
2021-11-13 09:56:02 +0100

    Formatting properties.py

--- decorators/properties.py
+++ decorators/properties.py
@@ -2,7 +2,7 @@ class MyClass:
 
     def __init__(self, a):
         self._a = a
-    
+
     @property
     def a(self):
         return self._a
@@ -13,13 +13,14 @@ class MyClass:
             self._a = new_a
         else:
             raise AttributeError("Attribute 'a' should be int!")
-    
+
     @a.deleter
     def a(self):
         del self._a
-    
+
+
 if __name__ == "__main__":
-    my_obj = MyClass(5)
+    my_obj = MyClass(6)
     print(my_obj.a)
     try:
         my_obj.a = 3.14
@@ -27,3 +28,4 @@ if __name__ == "__main__":
         print(f'Yep, this is not working: {exc}!')
     my_obj.a = 9
     print(my_obj.a)
+    print(my_obj.__getattribute__("_a"))
