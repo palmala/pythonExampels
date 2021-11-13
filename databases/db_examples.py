84dae2280764d3f05597dd95ac6201752b663910
palmala <palmailaszloprivate@gmail.com>
2021-11-13 17:16:44 +0100

    Fixing typos in db_examples

--- databases/db_examples.py
+++ databases/db_examples.py
@@ -2,18 +2,21 @@ import sqlite3
 import random
 
 if __name__ == "__main__":
-    print(f'Connecting to file: my_database.db')
+    # Connecting to db file
     db = sqlite3.connect('my_database.db')
+    # Opening cursor
     cur = db.cursor()
+    # Creating table 'test' if doesn't exist
     cur.execute("""
         CREATE TABLE IF NOT EXISTS test (
             id INTEGER PRIMARY KEY, string TEXT, number INTEGER
         )
     """)
-    print("Inserting row")
+    # Inserting new row
     cur.execute("INSERT INTO test (string, number) VALUES ('one', {})".format(random.randint(0, 100)))
-    print("Committing to db")
+    # Committing to DB
     db.commit()
+    # Printing out the contents of the table 'test'
     for row in cur.execute("SELECT * FROM test"):
         print(row)
     db.close()
