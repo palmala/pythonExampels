58d317f52adfa106128402b35e73ccec80e4725e
palmala <palmailaszloprivate@gmail.com>
2021-11-14 09:46:30 +0100

    Cleaning up escaping strings example

--- strings/escaping_strings.py
+++ strings/escaping_strings.py
@@ -1,10 +1,11 @@
-import string
 import json
 
 if __name__ == "__main__":
     simple_json = '{ "person": {"firstname": "John", "lastname": "Doe"}, "friends": [{' \
                   '"person": {"firstname": "Jane", "lastname": "Doe"}' \
                   '}]}'
-    print(simple_json)
+    print(f'Original string: {simple_json}')
+    escaped = simple_json.replace('"', '\\"')
+    print(f'Escaped string: {escaped}')
     parsed_json = json.loads(simple_json)
-    print(parsed_json)
+    print(f'Parsed json: {parsed_json}')
