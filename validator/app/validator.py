caba972e5896c3fcf7645a5566d572f5809e0fd6
palmala <palmala@users.noreply.github.com>
2022-03-19 22:32:46 +0100

    Adding json support and random element tests

--- validator/app/validator.py
+++ validator/app/validator.py
@@ -1,8 +1,8 @@
 import click
 import os
 import sys
-from collections import defaultdict
 import random
+import json
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current_directory)
@@ -46,20 +46,50 @@ def get_tests_for_tag(path_to_tests, tag):
     else:
         raise AttributeError(f"No tests for tag {tag}")
 
+
 @click.command(name='get_random_tests_per_tag')
 @click.argument('path_to_tests', type=click.Path(exists=True), required=True)
 @click.argument('number_of_tests', type=int, required=True)
-def get_random_tests_per_tag(path_to_tests, number_of_tests):
+@click.option('--untagged/--no-untagged', default=False)
+@click.option('--js/--no-js', default=False)
+def get_random_tests_per_tag(path_to_tests, number_of_tests, untagged, js):
+    result = dict()
     tests_per_tags = tags.get_tests_per_tags(path_to_tests, "md")
     for tag in tests_per_tags:
-        print(f"{tag}:")
-        if 
+        if not untagged and tag == "UNTAGGED":
+            continue
+        num = min([number_of_tests, len(tests_per_tags[tag])])
+        result[tag] = random.sample(tests_per_tags[tag], k=num)
+    _print_dict(js, result)
 
+    sys.exit(0)
+
+
+@click.command(name='get_tests_per_tags')
+@click.argument('path_to_tests', type=click.Path(exists=True), required=True)
+@click.option('--js/--no-js', default=False)
+def get_tests_per_tags(path_to_tests, js):
+    result = tags.get_tests_per_tags(path_to_tests, "md")
+    _print_dict(js, result)
+    sys.exit(0)
 
 
 cli.add_command(get_tags)
 cli.add_command(get_untagged)
 cli.add_command(get_tests_for_tag)
+cli.add_command(get_random_tests_per_tag)
+cli.add_command(get_tests_per_tags)
+
+
+def _print_dict(js, result):
+    if js:
+        print(json.dumps(result, indent=4))
+    else:
+        for k, v in result.items():
+            print(k)
+            for test_file in v:
+                print(f"\t{test_file}")
+
 
 if __name__ == "__main__":
     cli()
--- validator/tests/cli_tests.py
+++ validator/tests/cli_tests.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import json
 
 from click.testing import CliRunner
 
@@ -10,16 +11,23 @@ sys.path.append(parent)
 from app.validator import get_untagged
 from app.validator import get_tags
 from app.validator import get_tests_for_tag
+from app.validator import get_random_tests_per_tag
+from app.validator import get_tests_per_tags
 
 TEST_CASES_DIR = "test_cases"
 
 
 class CliTests(unittest.TestCase):
 
+    def setUp(self):
+        runner = CliRunner()
+        results = runner.invoke(get_tests_per_tags, [f"{TEST_CASES_DIR}", "--js"])
+        self.all_tests = json.loads(results.output)
+
     @staticmethod
     def test_get_untagged():
         runner = CliRunner()
-        result = runner.invoke(get_untagged, f"{TEST_CASES_DIR}/untagged")
+        result = runner.invoke(get_untagged, f"{TEST_CASES_DIR}")
         print(f"Output is {result.output}")
         assert (result.exit_code != 0)
         assert ("test1.md" in result.output)
@@ -39,6 +47,23 @@ class CliTests(unittest.TestCase):
         print(f"Output is {result.output}")
         assert (result.exit_code == 0)
         assert (all([test in result.output for test in ["test2.md", "test3.md", "test4.md"]]))
+
+    def test_get_random_tests(self):
+        runner = CliRunner()
+        NUM_TESTS = 2
+        result = runner.invoke(get_random_tests_per_tag, [TEST_CASES_DIR, str(NUM_TESTS), "--js"])
+        result_json = json.loads(result.output)
+        assert (result.exit_code == 0)
+        all_tags = set([tag for tag in self.all_tests if tag != "UNTAGGED"])
+        result_tags = set([tag for tag in result_json])
+        assert (all_tags == result_tags)
+        for tag in result_tags:
+            if len(self.all_tests[tag]) <= NUM_TESTS:
+                assert (len(result_json[tag]) == len(self.all_tests[tag]))
+                assert (set(result_json[tag]) == set(self.all_tests[tag]))
+            else:
+                assert (len(result_json[tag]) < len(self.all_tests[tag]))
+                assert (set(result_json[tag]).issubset(set(self.all_tests[tag])))
 
 
 if __name__ == '__main__':
