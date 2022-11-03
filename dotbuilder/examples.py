419fce0a0e4e8c3f787b83afee6ef06e3780aec6
palmala <palmailaszloprivate@gmail.com>
2022-11-03 20:50:52 +0100

    dotbuilder Adding examples about graph level instability

--- dotbuilder/examples.py
+++ dotbuilder/examples.py
@@ -13,6 +13,9 @@ def process_example(projects, name):
     subject = dot_builder(projects, name)
     instability = calculate_instability(subject)
     calculate_violations(subject, instability)
+    average_i = float(sum(list(instability.values()))) / len(subject.get_edges())
+    graph_i = average_i * len(subject.get_edge_list())
+    print(f"Name: {name}, Average I: {average_i}, Graph level I: {graph_i}")
     write_to_file(subject, FILENAMES.format(basedir=OUTPUT, filename=name))
     return subject
 
@@ -28,6 +31,7 @@ def main():
         'D': ['A']
     }
     process_example(projects, "maxI")
+
     projects = {
         'A': [],
         'B': ['A', 'C'],
@@ -37,12 +41,36 @@ def main():
     process_example(projects, "maxI2")
 
     projects = {
+        'A': [],
+        'B': ['A', 'C'],
+        'C': ['A', 'D'],
+        'D': ['A']
+    }
+    process_example(projects, "maxI3")
+
+    projects = {
         'A': ['B', 'C', 'D'],
         'B': [],
         'C': [],
         'D': []
     }
     process_example(projects, "minI")
+
+    projects = {
+        'A': ['B', 'C', 'D'],
+        'B': ['C'],
+        'C': [],
+        'D': []
+    }
+    process_example(projects, "minI2")
+
+    projects = {
+        'A': ['B', 'C', 'D'],
+        'B': ['C'],
+        'C': ['D'],
+        'D': []
+    }
+    process_example(projects, "minI3")
 
 
 if __name__ == "__main__":
