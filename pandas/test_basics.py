fa1bd46a3393d08c6300d87539209e5fce38d997
palmala <palmailaszloprivate@gmail.com>
2022-11-23 07:54:42 +0100

    pandas eliminating 'month' column

--- pandas/test_basics.py
+++ pandas/test_basics.py
@@ -4,7 +4,6 @@ from pandas import Timestamp
 from pandas.testing import assert_frame_equal
 
 import timestamp_lib
-import numpy
 
 
 class BasicTests(unittest.TestCase):
@@ -27,30 +26,30 @@ class BasicTests(unittest.TestCase):
     def test_merge_overlaps(self):
         # GIVEN
         data = [
-            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 14:00:00')},
-            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 08:00:00')},
-            {'month': '2022-01', 'from': Timestamp('2022-01-01 08:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
-            {'month': '2022-01', 'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
-            {'month': '2022-02', 'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 08:00:00')},
-            {'month': '2022-02', 'from': Timestamp('2022-02-01 08:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
-            {'month': '2022-02', 'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
+            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 14:00:00')},
+            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 08:00:00')},
+            {'from': Timestamp('2022-01-01 08:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
+            {'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
+            {'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 08:00:00')},
+            {'from': Timestamp('2022-02-01 08:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
+            {'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
 
         ]
         subject = pandas.DataFrame.from_dict(data)
 
         # WHEN
-        merged_overlaps = timestamp_lib.merge_overlaps(dataframe=subject, group_by="month")
+        merged_overlaps = timestamp_lib.merge_overlaps(dataframe=subject)
 
         # THEN
         self.assertFalse(merged_overlaps.empty)
         expected = pandas.DataFrame.from_dict([
-            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
-            {'month': '2022-01', 'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
-            {'month': '2022-02', 'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
-            {'month': '2022-02', 'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
+            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
+            {'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
+            {'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
+            {'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
         ])
-        diff = expected.compare(merged_overlaps)
-        self.assertTrue(diff.empty)
+        assert_frame_equal(expected, merged_overlaps.sort_values(['from']).reset_index(drop=True),
+                           check_index_type=False)
 
     def test_split_month_overflow(self):
         # GIVEN
@@ -59,6 +58,7 @@ class BasicTests(unittest.TestCase):
             {'from': Timestamp('2022-01-11 20:00:00'), 'to': Timestamp('2022-01-11 23:00:00')}
         ]
         subject = pandas.DataFrame.from_dict(data)
+        print(subject)
 
         # WHEN
         split_overflows = timestamp_lib.split_overflows(dataframe=subject)
@@ -70,4 +70,5 @@ class BasicTests(unittest.TestCase):
             {'from': Timestamp('2022-01-31 20:00:00'), 'to': Timestamp('2022-02-01 00:00:00')},
             {'from': Timestamp('2022-02-01 00:00:00'), 'to': Timestamp('2022-02-01 04:00:00')}
         ])
-        assert_frame_equal(expected, split_overflows, check_index_type=False)
+        assert_frame_equal(expected, split_overflows.sort_values(['from']).reset_index(drop=True),
+                           check_index_type=False)
