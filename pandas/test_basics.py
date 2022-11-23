458a75cf659c5f6043a81ba9a7b54d170da2cd6a
palmala <palmailaszloprivate@gmail.com>
2022-11-23 09:34:31 +0100

    Revert "pandas eliminating 'month' column"
    This reverts commit fa1bd46a3393d08c6300d87539209e5fce38d997.

--- pandas/test_basics.py
+++ pandas/test_basics.py
@@ -4,6 +4,7 @@ from pandas import Timestamp
 from pandas.testing import assert_frame_equal
 
 import timestamp_lib
+import numpy
 
 
 class BasicTests(unittest.TestCase):
@@ -26,30 +27,30 @@ class BasicTests(unittest.TestCase):
     def test_merge_overlaps(self):
         # GIVEN
         data = [
-            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 14:00:00')},
-            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 08:00:00')},
-            {'from': Timestamp('2022-01-01 08:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
-            {'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
-            {'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 08:00:00')},
-            {'from': Timestamp('2022-02-01 08:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
-            {'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
+            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 14:00:00')},
+            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 08:00:00')},
+            {'month': '2022-01', 'from': Timestamp('2022-01-01 08:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
+            {'month': '2022-01', 'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
+            {'month': '2022-02', 'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 08:00:00')},
+            {'month': '2022-02', 'from': Timestamp('2022-02-01 08:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
+            {'month': '2022-02', 'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
 
         ]
         subject = pandas.DataFrame.from_dict(data)
 
         # WHEN
-        merged_overlaps = timestamp_lib.merge_overlaps(dataframe=subject)
+        merged_overlaps = timestamp_lib.merge_overlaps(dataframe=subject, group_by="month")
 
         # THEN
         self.assertFalse(merged_overlaps.empty)
         expected = pandas.DataFrame.from_dict([
-            {'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
-            {'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
-            {'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
-            {'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
+            {'month': '2022-01', 'from': Timestamp('2022-01-01 04:00:00'), 'to': Timestamp('2022-01-01 16:00:00')},
+            {'month': '2022-01', 'from': Timestamp('2022-01-02 01:00:00'), 'to': Timestamp('2022-01-02 11:00:00')},
+            {'month': '2022-02', 'from': Timestamp('2022-02-01 04:00:00'), 'to': Timestamp('2022-02-01 12:00:00')},
+            {'month': '2022-02', 'from': Timestamp('2022-02-02 08:00:00'), 'to': Timestamp('2022-02-02 16:00:00')}
         ])
-        assert_frame_equal(expected, merged_overlaps.sort_values(['from']).reset_index(drop=True),
-                           check_index_type=False)
+        diff = expected.compare(merged_overlaps)
+        self.assertTrue(diff.empty)
 
     def test_split_month_overflow(self):
         # GIVEN
@@ -58,7 +59,6 @@ class BasicTests(unittest.TestCase):
             {'from': Timestamp('2022-01-11 20:00:00'), 'to': Timestamp('2022-01-11 23:00:00')}
         ]
         subject = pandas.DataFrame.from_dict(data)
-        print(subject)
 
         # WHEN
         split_overflows = timestamp_lib.split_overflows(dataframe=subject)
@@ -70,5 +70,4 @@ class BasicTests(unittest.TestCase):
             {'from': Timestamp('2022-01-31 20:00:00'), 'to': Timestamp('2022-02-01 00:00:00')},
             {'from': Timestamp('2022-02-01 00:00:00'), 'to': Timestamp('2022-02-01 04:00:00')}
         ])
-        assert_frame_equal(expected, split_overflows.sort_values(['from']).reset_index(drop=True),
-                           check_index_type=False)
+        assert_frame_equal(expected, split_overflows, check_index_type=False)
--- pandas/timestamp_lib.py
+++ pandas/timestamp_lib.py
@@ -1,27 +1,43 @@
-import pandas as pd
+import pandas
+from collections import defaultdict
 from copy import deepcopy
 
+import pandas as pd
 
-def detect_month_overflows(dataframe: pd.DataFrame, from_column: str = "from", to_column: str = "to") -> set:
+
+def detect_month_overflows(dataframe: pandas.DataFrame, from_column: str = "from", to_column: str = "to") -> set:
     result = set()
 
     for ind in dataframe.index:
-        from_date = pd.Timestamp(dataframe[from_column][ind])
-        to_date = pd.Timestamp(dataframe[to_column][ind])
+        from_date = pandas.Timestamp(dataframe[from_column][ind])
+        to_date = pandas.Timestamp(dataframe[to_column][ind])
         if from_date.month != to_date.month:
             result.add(tuple([from_date, to_date]))
 
     return result
 
 
-def merge_overlaps(dataframe: pd.DataFrame, from_column: str = "from", to_column: str = "to") -> pd.DataFrame:
-    result = []
-    to_dataframe = _merge_intervals_list(
-        [[row[from_column], row[to_column]] for row in dataframe.to_dict('records')])
-    for row in to_dataframe:
-        result.append({from_column: row[0], to_column: row[1]})
+def merge_overlaps(dataframe: pandas.DataFrame, group_by: str = "month", from_column: str = "from",
+                   to_column: str = "to") -> pandas.DataFrame:
+    grouped = _df_to_dict(dataframe, group_by, from_column, to_column)
 
-    return pd.DataFrame.from_dict(result)
+    to_dataframe = []
+    for key in grouped:
+        grouped[key] = _merge_intervals_list(grouped[key])
+        for interval in grouped[key]:
+            row = {group_by: key, from_column: interval[0], to_column: interval[1]}
+            to_dataframe.append(row)
+
+    result = pandas.DataFrame.from_dict(to_dataframe)
+    return result
+
+
+def _df_to_dict(dataframe, from_column, group_by, to_column):
+    grouped = defaultdict(list)
+    for ind in dataframe.index:
+        window = [dataframe[from_column][ind], dataframe[to_column][ind]]
+        grouped[dataframe[group_by][ind]].append(window)
+    return grouped
 
 
 def _merge_intervals_list(intervals: list) -> list:
@@ -36,19 +52,19 @@ def _merge_intervals_list(intervals: list) -> list:
     return result
 
 
-def split_overflows(dataframe: pd.DataFrame, from_column: str = "from", to_column: str = "to") -> pd.DataFrame:
+def split_overflows(dataframe: pandas.DataFrame, from_column: str = "from", to_column: str = "to") -> pandas.DataFrame:
     result = deepcopy(dataframe)
-    splits = pd.DataFrame()
+    splits = pandas.DataFrame()
     for ind in result.index:
-        from_date = pd.Timestamp(result[from_column][ind])
-        to_date = pd.Timestamp(result[to_column][ind])
+        from_date = pandas.Timestamp(result[from_column][ind])
+        to_date = pandas.Timestamp(result[to_column][ind])
         if from_date.month != to_date.month:
             split_date = to_date.replace(day=1, hour=0, minute=0)
             row = result.iloc[ind].to_dict()
             result.at[ind, to_column] = split_date
             row[from_column] = split_date
             if splits.empty:
-                splits = pd.DataFrame.from_dict([row])
+                splits = pandas.DataFrame.from_dict([row])
             else:
                 new_row_df = pd.DataFrame(row)
                 splits = pd.concat([splits, new_row_df], ignore_index=True)
@@ -56,4 +72,5 @@ def split_overflows(dataframe: pd.DataFrame, from_column: str = "from", to_colum
     if not splits.empty:
         result = pd.concat([splits, result], ignore_index=True)
 
+    result = result.sort_values([from_column]).reset_index(drop=True)
     return result
