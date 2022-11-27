46cefd9ad56a7599de160fa7884ed287433c36ab
Tóth Evelin <tothevelin@Toth-MacBook-Pro.local>
2022-11-27 10:15:54 +0100

    Refactor workday_counter and add unittests

---
+++ pandas/test_workday_counter.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import unittest
+from workday_counter import _get_country_name_for_region, _get_num_of_workdays_of_a_country, get_num_of_workdays
+
+class TestWorkdayCounter(unittest.TestCase):
+	
+	def test_get_country_name_for_region(self):
+		#GIVEN
+		ln_region = 'ln'
+		hk_region = 'hk'
+		ny_region = 'newyork'
+		tk_region = 'tokio'
+		invalid_region = 'invalid_region'
+		
+		#WHEN
+		actual_ln = _get_country_name_for_region(ln_region)
+		actual_hk = _get_country_name_for_region(hk_region)
+		actual_ny= _get_country_name_for_region(ny_region)
+		actual_tk = _get_country_name_for_region(tk_region)
+
+		#THEN
+		self.assertEqual(actual_ln, 'UnitedKingdom')
+		self.assertEqual(actual_hk, 'HongKong')
+		self.assertEqual(actual_ny, 'UnitedStates')
+		self.assertEqual(actual_tk, 'Japan')
+		self.assertRaises(ValueError, _get_country_name_for_region, invalid_region)
+			
+		
+	def test_get_num_of_workdays_of_a_country(self):
+		#GIVEN
+		#WHEN
+		actual_ln_workdays_2022_04 = _get_num_of_workdays_of_a_country('UnitedKingdom', 4, 2022)
+		actual_hk_workdays_2022_02 = _get_num_of_workdays_of_a_country('HongKong', 2, 2022)
+		actual_tk_workdays_2021_04 = _get_num_of_workdays_of_a_country('Japan', 4, 2021)
+		actual_ny_workdays_2020_07 = _get_num_of_workdays_of_a_country('UnitedStates', 7, 2020)
+		
+		#THEN
+		self.assertEqual(actual_ln_workdays_2022_04, 19)
+		self.assertEqual(actual_hk_workdays_2022_02, 17)
+		self.assertEqual(actual_tk_workdays_2021_04, 21)
+		self.assertEqual(actual_ny_workdays_2020_07, 22)
+
+
+	def test_get_num_of_workdays(self):
+		#GIVEN
+		#WHEN
+		actual_hk_workdays_2022_04 = get_num_of_workdays('hk', month=4, year=2022)
+		actual_ny_workdays_2022_04 = get_num_of_workdays('ny', month=4, year=2022)
+		actual_ln_workdays_2022_04 = get_num_of_workdays('ln', month=4, year=2022)
+		actual_tk_workdays_2022_04 = get_num_of_workdays('tk', month=4, year=2022)
+		the_smallest_num_of_workdays_amongst_regions = get_num_of_workdays(month=4, year=2022)
+		
+		
+		#THEN
+		self.assertEqual(actual_hk_workdays_2022_04, 18)
+		self.assertEqual(actual_ln_workdays_2022_04, 19)
+		self.assertEqual(actual_tk_workdays_2022_04, 20)
+		self.assertEqual(actual_ny_workdays_2022_04, 21)
+		self.assertEqual(the_smallest_num_of_workdays_amongst_regions, 18)
+
+	
+if __name__ == "__main__":
+	unittest.main()
--- pandas/workday_counter.py
+++ pandas/workday_counter.py
@@ -8,45 +8,30 @@ from datetime import date
 import holidays
 
 
-REGIONS = ['ln', 'ny', 'tk', 'hk']
+REGIONS = {('ln', 'london'): 'UnitedKingdom', ('ny', 'new_york', 'newyork'): 'UnitedStates', ('tk', 'tokio'): "Japan", ('hk', 'hong_kong', 'hongkong'): 'HongKong'}
 
-def _transform_region_to_country(region: str):
+
+def _get_country_name_for_region(region: str):
 	region = region.lower()
+	for reg in REGIONS:
+		if region in reg:
+			return REGIONS[reg]
+	raise ValueError(f"Region \"{region}\" is not valid. Please choose from these: {list(('/').join(region) for region in REGIONS)}")
 	
-	if region in ['tk', 'tokio']:
-		return 'Japan'
-	elif region in ['hk', 'hongkong', 'hong_kong']:		
-		return 'HongKong'
-	elif region in ['ny', 'newyork', 'new_york']:		
-		return 'UnitedStates'
-	elif region in ['ln', 'london']:		
-		return 'UnitedKingdom'
-	else:
-		raise ValueError(f"Region \"{region}\" is not valid. Please choose from these: {REGIONS}")
-		
 
-def _get_num_of_workdays(country: str, month: int, year: int):
+def _get_num_of_workdays_of_a_country(country: str, month: int, year: int):
 	holidays_res = holidays.country_holidays(country=country, years=year).keys()
 	holidays_of_month = [holiday for holiday in holidays_res if holiday.month == month]
-	
 	start = dt.date( year, month, 1)
 	end = (dt.date( year, month, 1) + timedelta(days=32)).replace(day=1)
-	
 	num_of_workdays = np.busday_count(start, end, holidays=holidays_of_month)
 	return num_of_workdays
 	
 	
 def get_num_of_workdays(region: Optional[str] = 'global', month: Optional[int] = datetime.now().month, year: Optional[int] = dt.datetime.now().year):
 	if region == 'global':
-		workdays_of_all_regions = [_get_num_of_workdays(_transform_region_to_country(reg), month, year) for reg in REGIONS]
+		workdays_of_all_regions = [_get_num_of_workdays_of_a_country(country, month, year) for reg, country in REGIONS.items()]
 		return min(workdays_of_all_regions)
 	else:
-		country = _transform_region_to_country(region)
-		return _get_num_of_workdays(country, month, year)
-		
-		
-if __name__ == '__main__':
-	print(f"global: {get_num_of_workdays()}")
-	print(f"LN : {get_num_of_workdays('ln')}")
-	print(f"HK Feb 2022: {get_num_of_workdays('hk', 2, 2022)}")
-	
+		country = _get_country_name_for_region(region)
+		return _get_num_of_workdays_of_a_country(country, month, year)
