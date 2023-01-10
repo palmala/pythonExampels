904527de345b968dbb56b76d7c8236dd024c6d6e
Tóth Evelin <tothevelin@Toth-MacBook-Pro.local>
2023-01-10 12:26:30 +0100

    Implement AttributeMissingChecker and write test

--- csv_checkers/checkers.py
+++ csv_checkers/checkers.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-
+import csv
+from collections import defaultdict
 
 class Checker(ABC):
 
@@ -7,6 +8,31 @@ class Checker(ABC):
     def __init__(self):
         pass
 
+
     @abstractmethod
     def check(self, row: dict) -> dict:
         pass
+    
+    
+    @staticmethod
+    def merge_issues_dicts(list_of_issues_dicts: list):
+        merged_issues = defaultdict(lambda: defaultdict(list))
+        for issues_dict in list_of_issues_dicts:
+            for key, value in issues_dict.items():
+                for project, issue in value.items():
+                    merged_issues[key][project].append(issue)
+        
+        return merged_issues
+
+
+class AttributeMissingChecker(Checker):
+    
+    def __init__(self, checking_column: str):
+        self.checking_attribute = checking_column
+        self.issues = defaultdict(dict)
+    
+    
+    def check(self, row: dict) -> dict:
+        if not row[self.checking_attribute]:
+            issue = {row['cat']: f"Missing {self.checking_attribute} info"}
+            self.issues[row['owner']].update(issue)
--- csv_checkers/test_checkers.py
+++ csv_checkers/test_checkers.py
@@ -8,22 +8,26 @@ class CheckerTests(unittest.TestCase):
 
     def test_checkers(self):
         # GIVEN
-        subject = AttributeMissingChecker()
+        subject = AttributeMissingChecker('owner')
 
         # WHEN / THEN
         self.assertTrue(isinstance(subject, Checker))
+
 
     def test_single_attribute(self):
         # GIVEN
         with open('data.csv', 'r') as data_csv:
             checkers = [AttributeMissingChecker("owner"), AttributeMissingChecker("sex")]
             cats = csv.DictReader(data_csv)
-            issues = defaultdict(dict)
 
             # WHEN
             for cat in cats:
-                issues.update(checkers.check(cat))
+                for checker in checkers:
+                    checker.check(cat)
+            
+            issues = Checker.merge_issues_dicts([checker.issues for checker in checkers])
+            
 
         # THEN
-        self.assertDictEqual(issues['Owner1'], [{'Cat3': ["Missing sex info"]}])
-        self.assertDictEqual(issues[None], [{'Cat2': ["Missing owner info"]}])
+        self.assertDictEqual(issues['Owner1'], {'Cat3': ['Missing sex info'], 'Cat4': ['Missing sex info']})
+        self.assertDictEqual(issues[""], {'Cat2': ["Missing owner info"], 'Cat5': ["Missing owner info"], "Cat6": ["Missing owner info", "Missing sex info"]})
