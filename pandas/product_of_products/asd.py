93c138def9a416dae861674085c2167d48f1588e
palmala <palmailaszloprivate@gmail.com>
2023-01-20 21:50:22 +0100

    pandas yes

--- pandas/product_of_products/asd.py
+++ pandas/product_of_products/asd.py
@@ -11,25 +11,25 @@ if __name__ == "__main__":
     ing = pd.read_csv('ingredients.csv')
     ing.to_sql('ingredients', conn, if_exists='replace')
 
-    # cur.execute("""
-    #     SELECT
-    #          P1.PRODUCT_NAME, P1.VERSION, P2.PRODUCT_NAME, P2.VERSION
-    #     FROM
-    #         ((products as P1
-    #         INNER JOIN ingredients ON P1.PRODUCT_ID=ingredients.PRODUCT_ID)
-    #         INNER JOIN products as P2 ON P2.PRODUCT_ID=ingredients.INGREDIENT_ID)
-    # """)
-
     cur.execute("""
         SELECT
-             PRODUCT_NAME, VERSION, MAX(PRODUCT_ID)
-        FROM 
-            products
-        GROUP BY PRODUCT_NAME
+             P1.PRODUCT_NAME, P1.VERSION, P2.PRODUCT_NAME, P2.VERSION
+        FROM
+            (((SELECT
+                 PRODUCT_NAME, VERSION, MAX(PRODUCT_ID) AS PRODUCT_ID
+            FROM
+                products
+            GROUP BY PRODUCT_NAME) as P1
+            INNER JOIN ingredients ON P1.PRODUCT_ID=ingredients.PRODUCT_ID)
+            INNER JOIN products as P2 ON P2.PRODUCT_ID=ingredients.INGREDIENT_ID)
     """)
 
     # cur.execute("""
-    #     SELECT PRODUCT_NAME, VERSION, MAX(PRODUCT_ID) FROM products GROUP BY PRODUCT_NAME
+    #     SELECT
+    #          PRODUCT_NAME, VERSION, MAX(PRODUCT_ID)
+    #     FROM
+    #         products
+    #     GROUP BY PRODUCT_NAME
     # """)
 
     rows = cur.fetchall()
