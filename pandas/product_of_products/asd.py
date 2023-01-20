6be55441fe2666eeb6831c499c07ea9518215887
palmala <palmailaszloprivate@gmail.com>
2023-01-20 21:34:30 +0100

    pandas product of products

---
+++ pandas/product_of_products/asd.py
@@ -0,0 +1,26 @@
+import sqlite3
+import pandas as pd
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('file:cachedb?mode=memory&cache=shared')
+    cur = conn.cursor()
+
+    prod = pd.read_csv('products.csv')
+    prod.to_sql("products", conn, if_exists='replace')
+
+    ing = pd.read_csv('ingredients.csv')
+    ing.to_sql('ingredients', conn, if_exists='replace')
+
+    cur.execute("""
+        SELECT
+             P1.PRODUCT_NAME, P1.VERSION, P2.PRODUCT_NAME, P2.VERSION
+        FROM 
+            products as P1, ingredients as I, products as P2
+        WHERE
+            P1.PRODUCT_ID=I.PRODUCT_ID
+            AND
+            P2.PRODUCT_ID=I.INGREDIENT_ID
+    """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
